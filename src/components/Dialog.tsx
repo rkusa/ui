bbf44e00acb1ba5dc4c14b17e620e17ace7c0792
Markus Ast <m@rkusa.st>
2020-01-28 16:48:14 +0100

    move Dialog styled components to bottom of file

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -2,33 +2,7 @@ import React, { ReactNode, useCallback } from "react";
 import FocusLock from "react-focus-lock";
 import { RemoveScroll } from "react-remove-scroll";
 import styled from "styled-components";
-
-const Backdrop = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-  z-index: 999;
-`;
-
-const DialogContainer = styled.div`
-  position: fixed;
-  width: 640px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: ${props => props.theme.palette.white};
-  margin: auto;
-  z-index: 1000;
-  padding: 16px;
-  box-sizing: border-box;
-
-  @media (max-width: 1024px) {
-    width: 100vw;
-  }
-`;
+import Portal from "./Portal";
 
 export default function Dialog({
   className,
@@ -75,3 +49,30 @@ interface DialogProps {
   children?: ReactNode;
   onDismiss?: () => void;
 }
+
+const Backdrop = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  z-index: 999;
+`;
+
+const DialogContainer = styled.div`
+  position: fixed;
+  width: 640px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${props => props.theme.palette.white};
+  margin: auto;
+  z-index: 1000;
+  padding: 16px;
+  box-sizing: border-box;
+
+  @media (max-width: 1024px) {
+    width: 100vw;
+  }
+`;
