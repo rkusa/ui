4c876ee983a797e3869a57686aaf74573bbb9f43
Markus Ast <m@rkusa.st>
2020-02-14 09:11:54 +0100

    add transform option to useFormState

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -1,19 +1,27 @@
 import { useState, useCallback, useEffect } from "react";
 
-export default function useFormState<S>(initialState: S | (() => S)) {
+interface UseFormStateOpts<S> {
+  transform?: (val: S) => S
+}
+
+export default function useFormState<S>(initialState: S | (() => S), opts?: UseFormStateOpts<S>) {
   const [value, setValue] = useState(initialState);
+
   const onChange = useCallback(e => {
+    const transform = opts?.transform ?? (v => v)
     if (e && typeof e === "object" && e.target instanceof HTMLElement) {
-      setValue(e.target.value);
+      setValue(transform(e.target.value));
     } else {
-      setValue(e);
+      setValue(transform(e));
     }
-  }, []);
+  }, [opts?.transform]);
+
   useEffect(() => {
     if (typeof initialState !== "function" && initialState !== value) {
       setValue(initialState);
     }
   }, [initialState]);
+
   return { value, onChange };
 }
 
