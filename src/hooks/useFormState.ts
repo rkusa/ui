894338e6f4d13bd414e70a0a8ee54c4026527804
Markus Ast <m@rkusa.st>
2020-04-20 08:41:35 +0200

    apply prettier v2

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -1,20 +1,26 @@
 import { useState, useCallback, useEffect } from "react";
 
 interface UseFormStateOpts<S> {
-  transform?: (val: S) => S
+  transform?: (val: S) => S;
 }
 
-export default function useFormState<S>(initialState: S | (() => S), opts?: UseFormStateOpts<S>) {
+export default function useFormState<S>(
+  initialState: S | (() => S),
+  opts?: UseFormStateOpts<S>
+) {
   const [value, setValue] = useState(initialState);
 
-  const onChange = useCallback(e => {
-    const transform = opts?.transform ?? (v => v)
-    if (e && typeof e === "object" && e.target instanceof HTMLElement) {
-      setValue(transform(e.target.value));
-    } else {
-      setValue(transform(e));
-    }
-  }, [opts?.transform]);
+  const onChange = useCallback(
+    e => {
+      const transform = opts?.transform ?? (v => v);
+      if (e && typeof e === "object" && e.target instanceof HTMLElement) {
+        setValue(transform(e.target.value));
+      } else {
+        setValue(transform(e));
+      }
+    },
+    [opts?.transform]
+  );
 
   useEffect(() => {
     if (typeof initialState !== "function" && initialState !== value) {
