21510be59cea170238a0ad4d06d4ab54bb8d3d0c
Markus Ast <m@rkusa.st>
2020-05-18 17:08:15 +0200

    export Spinner component

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ export {
 export { default as useStore, Store } from "./hooks/useStore";
 
 // components
-
 export { default as Dialog } from "./components/Dialog";
 export { default as SideSheet } from "./components/SideSheet";
+export { default as Spinner} from "./components/Spinner";
 export { VStack, HStack } from "./components/Stack";
 
 // other
